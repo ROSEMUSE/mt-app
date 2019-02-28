44da21769ae9f2e40e9cc836219fd21ff97178b1
james <13562987549@sina.cn>
2019-02-28 18:04:35 +0800

    公共头部组件页面编写

---
+++ components/public/header/geo.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="m-geo">
+    <i class="el-icon-location">北京市</i>
+    <nuxt-link class="changCity" to="/changCity">切换城市</nuxt-link>
+    [香河 廊坊 燕郊]
+</div>
+    
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+
+</style>
+
+
